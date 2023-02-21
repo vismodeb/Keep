14167e68c15e34405d1d8f668fb9477deb19b84c
vismo_dev <105719010+vismodeb@users.noreply.github.com>
2023-02-21 20:53:48 +0600

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+   // Mobile Menu Icon
+   $(document).ready(function(){
+       $('#nav-icon4').click(function(){
+           $(this).toggleClass('open');
+           $('.header-nav nav').slideToggle();
+       });
+   });
+});
